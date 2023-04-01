049e11c419f22652aba8372e321b9a25e6892ab5
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-04-01 19:09:36 +0900

    .

--- controller/Screen.py
+++ controller/Screen.py
@@ -6,6 +6,6 @@ templates = Jinja2Templates(directory="FrontSide/templates")
 
 ScreenRoute = APIRouter(prefix="",tags=["Screens"])
 
-@ScreenRoute.get("/")
+@ScreenRoute.get("/test")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("test.html", {"request": request})
