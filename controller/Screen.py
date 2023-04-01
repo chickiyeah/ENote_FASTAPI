e9ea26c4e3ae601b3a90b1cdab9bbd5a54f55ad7
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-04-01 18:57:38 +0900

    프론트단

---
+++ controller/Screen.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates # html 템플레이트 로더
+templates = Jinja2Templates(directory="FrontSide/templates")
+
+ScreenRoute = APIRouter(prefix="",tags=["Screens"])
+
+@ScreenRoute.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
--- main.py
+++ main.py
@@ -3,12 +3,33 @@ from fastapi.security import OAuth2PasswordBearer
 import crypto
 import sys
 sys.modules['Crypto'] = crypto
+
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
+
+
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://3.34.125.70:83",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-from controller import userapi, papagoapi, noteapi
+from controller import userapi, papagoapi, noteapi, Screen
 
 app.include_router(userapi.userapi)
 app.include_router(papagoapi.papagoapi)
 app.include_router(noteapi.noteapi)
+app.include_router(Screen.ScreenRoute)
