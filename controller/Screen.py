d96e87e3d650da7aece216b8d94ef7bcaf73278d
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2023-04-04 18:39:58 +0900

    robots.txt

--- controller/Screen.py
+++ controller/Screen.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request
 
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates # html 템플레이트 로더
 templates = Jinja2Templates(directory="FrontSide/templates")
 
@@ -13,3 +13,8 @@ async def index(request: Request):
 @ScreenRoute.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@ScreenRoute.get("/robots.txt", response_class=PlainTextResponse)
+async def robots(request: Request):
+    data = """User-agent: *\nDisallow: /api\nDisallow: /docs\nAllow: /"""
+    return data
